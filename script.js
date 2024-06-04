100366ea5db6ae04f616f55653554373d1cc53a5
Jason Alvarado <jayxalvarado@gmail.com>
2024-06-03 18:02:00 -0700

    Finished stand, computerWinner, showWinner functions

--- script.js
+++ script.js
@@ -35,12 +35,15 @@ let blackjackGame = {
     A: [1, 11],
   },
 
-  //status
+  //score status
   wins: 0,
   losses: 0,
   draws: 0,
+
+  //status of the game
   isStand: false,
   isTurnsOver: false,
+
   //prevent button from being clicked after card is drawn
   pressOnce: false,
 };
@@ -54,16 +57,22 @@ const DEALER = blackjackGame["dealer"];
 //audio
 const hitSound = new Audio("sounds/swish.m4a");
 const winSound = new Audio("sounds/cash.mp3");
-const lossSound = new Audio("sounds/aww.mp3");
+const loseSound = new Audio("sounds/aww.mp3");
 
 //card dynamic size for all screens
 let windowWidth = window.screen.width;
 let windowHeight = window.screen.height;
 let winner;
 
+// Hit event listener
 document
   .querySelector("#blackjack-hit-button")
   .addEventListener("click", blackjackHit);
+
+// Stand event listener
+document
+  .querySelector("#blackjack-stand-button")
+  .addEventListener("click", blackjackStand);
 
 //checks if blackjackGame status is false. If true, it will draw a random card from the cards array with the randomCard function
 function blackjackHit() {
@@ -145,7 +154,7 @@ function updateScore(card, activePlayer) {
   }
 }
 
-// Show score
+// Show Score
 function showScore(activePlayer) {
   if (activePlayer["score"] > 21) {
     //display bust if score is over 21 by targeting the score span id
@@ -156,4 +165,101 @@ function showScore(activePlayer) {
     document.querySelector(activePlayer["scoreSpan"]).textContent =
       activePlayer["score"];
   }
+}
+
+// Stand button function
+function blackjackStand() {
+  if (blackjackGame.pressOnce === false) {
+    // stands === true if play has not made a move yet
+    blackjackGame["isStand"] = true;
+    //helps assign the equal number of cards given to both computer and player
+    let yourImages = document
+      .querySelector("#your-box")
+      .querySelectorAll("img");
+
+    for (let i = 0; i < yourImages.length; i++) {
+      let card = randomCard();
+      showCard(card, DEALER);
+      updateScore(card, DEALER);
+      showScore(DEALER);
+    }
+
+    //tells computer that the player has already made a move (stand)
+    blackjackGame["isTurnsOver"] = true;
+
+    //computes and shows the winner
+    computeWinner();
+    showWinner(winner);
+  }
+  //ends turn
+  blackjackGame.pressOnce = true;
+}
+
+//Score tracker
+function computeWinner() {
+  //First if statement
+  //if player is 21 or less
+  if (YOU["score"] <= 21) {
+    //condition: higher score than dealer or when dealer busts but you're 21 or under
+    if (YOU["score"] > DEALER["score"] || DEALER["score"] > 21) {
+      winner = YOU;
+    }
+    //condition if less than dealer
+    else if (YOU["score"] < DEALER["score"]) {
+      winner = DEALER;
+    }
+    //condition if both players have the same score
+    else if (YOU["score"] === DEALER["score"]) {
+      winner = "draw";
+    }
+    //condition if player busts but dealer doesn't
+  } else if (YOU["score"] > 21 && DEALER["score"] <= 21) {
+    winner = DEALER;
+    //condition if both player and dealer bust. No winner is chosen
+  } else if (YOU["score"] > 21 && DEALER["score"] > 21) {
+    winner = "None";
+  }
+  //return winner
+  return winner;
+}
+
+//Show Winner Display tracker with dynamic color
+function showWinner() {
+  let message, messageColor;
+
+  if (winner === YOU) {
+    message = "You won!";
+    messageColor = "#00e676";
+    //dyanmic score tracker
+    document.querySelector("#wins").textContent = blackjackGame["wins"] += 1;
+    winSound.play();
+  }
+
+  if (winner === DEALER) {
+    message = "You lost!";
+    messageColor = "red";
+    //dyanmic score tracker
+    document.querySelector("#losses").textContent = blackjackGame[
+      "losses"
+    ] += 1;
+    loseSound.play();
+  }
+
+  if (winner === "Draw") {
+    message = "Draw Game!";
+    messageColor = "yellow";
+    //dyanmic score tracker
+    document.querySelector("#draws").textContent = blackjackGame["draws"] += 1;
+    loseSound.play();
+  }
+
+  if (winner === "None") {
+    message = "Both busted!";
+    messageColor = "orange";
+    loseSound.play();
+  }
+
+  //targeting DOM element for winner message
+  document.querySelector("#blackjack-result").textContent = message;
+  document.querySelector("#blackjack-result").style.color = messageColor;
 }
